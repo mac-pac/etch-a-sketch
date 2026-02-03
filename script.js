fccfe2634112cfa7035e599ce3909ce1b785e90a
mac-pac <marcuscave0112@gmail.com>
2026-02-04 08:50:40 +1100

    Created canvas and interactive coloring via JS.
    Allows user to sketch within the canvas.

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('#container');
+
+const canvasResolution = 16 * 16;
+const canvasSize = 800;
+
+container.style.width = canvasSize + 'px';
+container.style.height = canvasSize + 'px';
+
+let pixelSize = (canvasSize/16) + "px";
+console.log(pixelSize)
+
+function generateCanvas() {
+
+    for (let i = 0; i < canvasResolution; i++){
+        const pixel = document.createElement('div');
+
+        pixel.classList.add('pixel');
+        pixel.style.width = pixelSize;
+        pixel.style.height = pixelSize;
+        container.append(pixel);
+    };
+    
+};
+
+generateCanvas();
+
+const pixels = document.querySelectorAll('#container div');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+        sketchPxl(pixel);
+    });
+    
+});
+
+function sketchPxl(pixel) {
+    pixel.style["background-color"] = "blue";
+    console.log(pixel);
+
+};
